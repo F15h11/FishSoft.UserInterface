837c24947a0bd870201a87917ccf8724e3bb6c8f
Daniel <147787037+F15h11@users.noreply.github.com>
2023-12-14 13:57:28 +0100

    Added project 0.1.x

---
+++ FishSoft.UserInterface/GetUserInput.cs
@@ -0,0 +1,54 @@
+﻿namespace FishSoft.UserInterface {
+    public static class GetUserInput {
+        public static string StringInput(string instruction, bool inputInSameLine, bool newLine) {
+            if(inputInSameLine) {
+                if(newLine) {
+                    Console.Out.Write($"\n{instruction}");
+                }
+                else {
+                    Console.Out.Write(instruction);
+                }
+                string? output = Console.ReadLine();
+                if(output != null) {
+                    return output;
+                }
+                else {
+                    return "";
+                }
+            }
+            else {
+                if(newLine) {
+                    Console.Out.Write($"\n{instruction}\n");
+                }
+                else {
+                    Console.Out.Write($"{instruction}\n");
+                }
+                string? output = Console.ReadLine();
+                if(output != null) {
+                    return output;
+                }
+                else {
+                    return "";
+                }
+            }
+        }
+        public static bool KeyInput(string instruction, ConsoleKey keyTrue, ConsoleKey keyFalse) {
+            Console.Out.Write("\n");
+            Console.Out.Write(instruction);
+            Console.Out.Write("\n");
+            while(true) {
+                ConsoleKey input = Console.ReadKey().Key;
+
+                if(input == keyTrue) {
+                    return true;
+                }
+                if(input == keyFalse) {
+                    return false;
+                }
+                else {
+                    continue;
+                }
+            }
+        }
+    }
+}
